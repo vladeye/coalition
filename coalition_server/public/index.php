b8863ef38416e16be81ebdcd4abe819e6f57ed70
vladeye <vladimirodriguez@gmail.com>
2022-02-24 23:56:19 -0500

    no cv

--- coalition_server/public/index.php
+++ coalition_server/public/index.php
@@ -1,9 +1,40 @@
 <?php
 
-use App\Kernel;
+include (dirname(__DIR__)."/src/Controller/InventoryController.php");
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+use Src\Controller\InventoryController;
+
+
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Set-Cookie: HttpOnly;Secure;SameSite=None");
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = explode( '/', $uri );
+
+// all of our endpoints start with /entity
+// everything else results in a 404 Not Found
+if ($uri[1] !== 'inventory') {
+    header("HTTP/1.1 404 Not Found");
+    exit();
+}
+
+// the id must be a number and is optional:
+$Id = null;
+
+if (isset($_GET['id'])) {
+    $Id = (int) $_GET['id'];
+}
+
+
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+
+$controller = new InventoryController($requestMethod, $Id);
+$controller->processRequest();
+
+//echo $requestMethod;
---
+++ coalition_server/src/Controller/InventoryController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Src\Controller;
+
+include (dirname(__DIR__)."/Gateways/InventoryGateway.php");
+use Src\Gateways\InventoryGateway;
+
+class InventoryController {
+    private $requestMethod;
+    private $Id;
+
+    private $inventoryGateway;
+
+
+    public function __construct($requestMethod, $Id)
+    {
+
+        $this->requestMethod = $requestMethod;
+        $this->Id = $Id;
+
+        $this->inventoryGateway = new InventoryGateway();
+
+    }
+
+    public function processRequest()
+    {
+        switch ($this->requestMethod) {
+            case 'GET':
+                if ($this->Id) {
+                    $response = $this->getInventory($this->Id);
+                } else {
+                    $response = $this->getAllInventory();
+                };
+                break;
+            case 'POST':
+                $response = $this->createInventoryFromRequest();
+                break;
+            case 'PUT':
+                $response = $this->updateInventoryFromRequest();
+                break;
+            case 'DELETE':
+                $response = $this->deleteInventory($this->Id);
+                break;
+            default:
+                $response = $this->notFoundResponse();
+                break;
+        }
+        header($response['status_code_header']);
+        if ($response['body']) {
+            echo $response['body'];
+        }
+    }
+
+    private function getAllInventory()
+    {
+        $result = $this->inventoryGateway->findAll();
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode($result);
+        return $response;
+    }
+
+    private function getInventory($id)
+    {
+        $result = $this->inventoryGateway->find($id);
+        if (! $result) {
+            return $this->notFoundResponse();
+        }
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode($result);
+        return $response;
+    }
+
+    private function notFoundResponse()
+    {
+        $response['status_code_header'] = 'HTTP/1.1 404 Not Found';
+        $response['body'] = null;
+        return $response;
+    }
+
+    private function createInventoryFromRequest()
+    {
+        $input = (array) json_decode(file_get_contents('php://input'), TRUE);
+        $result = $this->inventoryGateway->createInventory($input);
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode($result);
+        return $response;
+    }
+
+    private function updateInventoryFromRequest(){
+        $input = (array) json_decode(file_get_contents('php://input'), TRUE);
+        $result = $this->inventoryGateway->updateInventory($input);
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode($result);
+        return $response;
+    }
+
+    private function deleteInventory($Id){
+        $result = $this->inventoryGateway->deleteInventory($Id);
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode($result);
+        return $response;
+    }
+
+}
